880aed6119c1e52af79db0536a41e0c69950ebf3
ridlo <ridlo.w.wibowo@gmail.com>
2016-08-31 22:13:24 +0700

    update compareImage

--- src/compareImage.py
+++ src/compareImage.py
@@ -2,7 +2,7 @@
 This program aims to overplot images from different surveys and/or from our work.
 
 HISTORY:
-	2016.08.30:
+    2016.08.30:
         - first shot
 
 RUN:
@@ -29,70 +29,92 @@ __version__="0.1"
 
 
 class CompareImage:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def read_data_points(self, ifile):
-		points = np.genfromtxt(ifile, names=True)
-		ra = points['alpha']
-		dec = points['dec']
-		fwhm = points['FWHM']
+    def read_data_points(self, ifile):
+        points = np.genfromtxt(ifile, names=True)
+        ra = points['alpha']
+        dec = points['dec']
+        fwhm = points['FWHM']
 
-		return ra, dec, fwhm
-
-
-	def get_images(self, pos, radius, surveys, savefits=True):
-		images = SkyView.get_images(position=pos, coordinates='icrs', survey=surveys, radius=radius*u.arcmin, pixels=1000)
-		if savefits:
-			for i, s in enumerate(surveys):
-				images[i].writeto(s+'.fits', clobber=True) # overwrite existing file
-
-		return images
+        return ra, dec, fwhm
 
 
-	def overplot_points_to_image(self, data, image, show=True, savefig=True, ofile='overplot.png'):
-		ra, dec, fwhm = data
-		#image = fits.open(fits_file)
+    def get_images(self, pos, radius, surveys, savefits=True, dirname='images/'):
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
 
-		fig = aplpy.FITSFigure(image)
-		
-		fig.show_grayscale()
-		#fig.show_colorscale(cmap='gist_heat')
-		#fig.show_contour(colors='white')
+            images = SkyView.get_images(position=pos, coordinates='icrs', survey=surveys, radius=radius*u.arcmin, pixels=1000)
 
-		fig.show_markers(ra, dec, edgecolor='blue', facecolor='none', marker='o', s=fwhm*200, alpha=1.0)
-		if savefig:
-			fig.save(ofile)
+            if savefits:
+                for i, s in enumerate(surveys):
+                    images[i].writeto(dirname+s+'.fits', clobber=True) # overwrite existing file
 
-		if show:
-			plt.show(); plt.close()
+        else:
+            images = []
+            for i, s in enumerate(surveys):
+                filename = dirname+s+'.fits'
 
-		image.close()
+                if not os.path.isfile(filename):
+                    img = SkyView.get_images(position=pos, coordinates='icrs', survey=[s], radius=radius*u.arcmin, pixels=1000)
+                    images.append(img[0])
+                    if savefits:
+                        images[i].writeto(filename, clobber=True) # overwrite existing file
+                else:
+                    images.append(fits.open(filename))
+
+
+
+        return images
+
+
+    def overplot_points_to_image(self, data, image, show=True, savefig=True, dirname='images/', ofile='overplot.png'):
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        
+        ra, dec, fwhm = data
+        #image = fits.open(fits_file)
+
+        fig = aplpy.FITSFigure(image)
+        
+        fig.show_grayscale()
+        #fig.show_colorscale(cmap='gist_heat')
+        #fig.show_contour(colors='white')
+
+        fig.show_markers(ra, dec, edgecolor='blue', facecolor='none', marker='o', s=fwhm*200, alpha=1.0)
+        if savefig:
+            fig.save(dirname+ofile)
+
+        if show:
+            plt.show(); plt.close()
+
+        image.close()
 
 
 
 if __name__ == "__main__":
-	ifile = 'point_source.dat'
-	position = coordinates.SkyCoord('05h22m57.98465s', '-36d27m30.8509s', frame='icrs') 
-	
-	CI = CompareImage()
-	
-	points = CI.read_data_points(ifile)
+    ifile = 'point_source.dat'
+    position = coordinates.SkyCoord('05h22m57.98465s', '-36d27m30.8509s', frame='icrs') 
+    
+    CI = CompareImage()
+    
+    points = CI.read_data_points(ifile)
 
-	surveys = ['DSS', '2MASS-K', '2MASS-J', 'WISE 22', 'WISE 3.4']
-	
-	images = CI.get_images(position, 1.0, surveys)
+    surveys = ['DSS', '2MASS-K', '2MASS-J', 'WISE 22', 'WISE 3.4', "Fermi 1"]
+    
+    images = CI.get_images(position, 1.0, surveys) # 1 arcmin
 
-	for i, s in enumerate(surveys):
-		# images = fits.open(s+'.fits')
-		CI.overplot_points_to_image(points, images[i], ofile=s+'_and_pointSources.png')
+    for i, s in enumerate(surveys):
+        # images = fits.open(s+'.fits')
+        CI.overplot_points_to_image(points, images[i], ofile=s+'_and_pointSources.png')
 
 
 
-	# surv = ['Fermi 5', 'HRI', 'DSS']
-	# etaimages = SkyView.get_images(position='Eta Carinae', survey=surv)
-	# for i, s in enumerate(surv):
-	# 	etaimages[i].writeto(s+'.fits', clobber=True)
-	# 	fig = aplpy.FITSFigure(etaimages[i])
-	# 	fig.show_grayscale()
-	# 	fig.save('eta_'+s+'.png')
+    # surv = ['Fermi 5', 'HRI', 'DSS']
+    # etaimages = SkyView.get_images(position='Eta Carinae', survey=surv)
+    # for i, s in enumerate(surv):
+    #   etaimages[i].writeto(s+'.fits', clobber=True)
+    #   fig = aplpy.FITSFigure(etaimages[i])
+    #   fig.show_grayscale()
+    #   fig.save('eta_'+s+'.png')
